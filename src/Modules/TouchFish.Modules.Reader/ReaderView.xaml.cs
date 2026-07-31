755767094195c530ee90aa95d0f31021f393fca9
limelisest <limelisest@gmail.com>
2026-07-31 09:35:10 +0000

    fix: scroll chapter list to reading position

--- src/Modules/TouchFish.Modules.Reader/ReaderView.xaml.cs
+++ src/Modules/TouchFish.Modules.Reader/ReaderView.xaml.cs
@@ -1,4 +1,6 @@
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Threading;
 
 namespace TouchFish.Modules.Reader;
 
@@ -7,5 +9,28 @@ public partial class ReaderView : UserControl
     public ReaderView()
     {
         InitializeComponent();
+        Loaded += (_, _) => ScheduleSelectedChapterScroll();
+    }
+
+    private void ChapterList_OnSelectionChanged(object sender, SelectionChangedEventArgs e) =>
+        ScheduleSelectedChapterScroll();
+
+    private void ScheduleSelectedChapterScroll()
+    {
+        var selectedChapter = ChapterList.SelectedItem;
+        if (selectedChapter is null)
+        {
+            return;
+        }
+
+        _ = Dispatcher.BeginInvoke(
+            DispatcherPriority.Loaded,
+            new Action(() =>
+            {
+                if (IsVisible && ReferenceEquals(ChapterList.SelectedItem, selectedChapter))
+                {
+                    ChapterList.ScrollIntoView(selectedChapter);
+                }
+            }));
     }
 }
